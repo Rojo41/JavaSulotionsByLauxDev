fbc58494437f4381842e4f9846bfb5471bec401d
Laux Codes <115506131+Rojo41@users.noreply.github.com>
2023-09-20 21:52:01 +0800

    Update ParallelOrIntersecting.java

--- ParallelOrIntersecting.java
+++ ParallelOrIntersecting.java
@@ -4,7 +4,7 @@ public class ParallelOrIntersecting {
 //LauxDev
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		float firstLineSlope, secondLineSlope;
+		float firstLineSlope = 0, secondLineSlope = 0;
 		
 		System.out.println("Enter Cords of First Line(x1 y1 x2 y2): ");
 		int firstLinex1 = input.nextInt(), firstLiney1 = input.nextInt(),
@@ -13,10 +13,12 @@ public class ParallelOrIntersecting {
 		int secondLinex1 = input.nextInt(), secondLiney1 = input.nextInt(),
 				secondLinex2 = input.nextInt(),secondLiney2 = input.nextInt();
 		
+		if(firstLinex2 - firstLinex1 != 0) {	
 		firstLineSlope = (firstLiney2 - firstLiney1) / (firstLinex2 - firstLinex1);
+			}
+		if( secondLinex2 - secondLinex1 != 0) {
 		secondLineSlope = (secondLiney2 - secondLiney1) / (secondLinex2 - secondLinex1);
-		System.out.println(firstLineSlope);
-		System.out.println(secondLineSlope);
+		}
 		if(firstLineSlope == secondLineSlope) {
 			System.out.println("Paralell");
 		}
@@ -26,7 +28,7 @@ public class ParallelOrIntersecting {
 		if(firstLinex1 == firstLinex2 && secondLinex1 == secondLinex2) {
 			System.out.print(" - Both Vertical");
 		}
-		else if(firstLiney1 == firstLiney2 || secondLiney1 == secondLiney2) {
+		else if(firstLinex1 == firstLinex2 || secondLinex1 == secondLinex2) {
 			System.out.print(" - One Vertical");
 		}
 		
